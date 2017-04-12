b93959e0b7b8a5df98628b4b3d11332d718648fe
Arpan Tolat <arpantolat30@gmail.com>
2017-04-12 16:28:36 -0500

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 HEROKU_PID=$(pidof -s heroku)
-awk '$1=$1' FS=',' OFS='\n' ./ServerList.txt>>newlist.txt
+awk '$1=$1' FS=',' OFS='\n' ./ServerList.txt>>calritystate.txt
 
 #Check for new approval
-grep -vxFf createdservers.txt newlist.txt>difference.txt
+grep -vxFf shellstate.txt gelstate.txt>difference.txt
 
 #Create array of new approvals
 declare -a scripts=($(cat difference.txt))
@@ -16,12 +16,15 @@ else
 	for varname in "${scripts[@]}"
 	do
             heroku create ${varname}>./${varname}log.txt
-            #git remote <name><url>
+            #git remote add <name> <url>
             #git push heroku master
             echo "heroku create ${varname}">${varname}.sh
             chmod 755 ${varname}.sh
-			./${varname}.sh
-			echo "${varname}">>createdservers.txt
+			echo "${varname}">>shellstate.txt
+            heroku info --shell ${varname}>${varname}info.txt
+            awk '$1=$1' FS='=' OFS='\n' ${varname}info.txt>${varname}info.txt
+            paste -d, -s ${varname}info.txt>${varname}info.csv
 	done
 fi
-				
+
+awk '$1=$1' FS=',' OFS='\n' ./shellstate.txt>>.txt
